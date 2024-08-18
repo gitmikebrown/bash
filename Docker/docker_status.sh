bd4a11a9297881536ee1811c8bf6edc34075a409
mjbrow17 <mike.j.brown@outlook.com>
2024-08-18 13:49:27 -0700

    turned docker status into a function

--- Docker/docker_status.sh
+++ Docker/docker_status.sh
@@ -5,18 +5,22 @@
 ################################################################################################
 
 
+function docker_status(){
+    text_Green='\033[0;32m';
+    text_reset='\033[0m';
+    clear;
+    printf "\n\n${text_Green}-Networks-----------------------------------------------------${text_reset}\n\n";
+    sudo docker network ls; 
+    printf "\n\n${text_Green}-Volumes-----------------------------------------------------${text_reset}\n\n";
+    sudo docker volume ls; 
+    printf "\n${text_Green}-Images--------------------------------------------------------${text_reset}\n\n";
+    sudo docker images; 
+    printf "\n${text_Green}-Containers - All------------------------------------------------${text_reset}\n\n";
+    sudo docker ps -a;
+    printf "\n${text_Green}-Containers - Currently Running-----------------------------------${text_reset}\n\n";
+    sudo docker ps;
+    printf "\n\n";
 
-text_Green='\033[0;32m'
-text_reset='\033[0m'
-clear;
-printf "\n\n${text_Green}-Networks-----------------------------------------------------${text_reset}\n\n";
-sudo docker network ls; 
-printf "\n\n${text_Green}-Volumes-----------------------------------------------------${text_reset}\n\n";
-sudo docker volume ls; 
-printf "\n${text_Green}-Images--------------------------------------------------------${text_reset}\n\n";
-sudo docker images; 
-printf "\n${text_Green}-Containers - All------------------------------------------------${text_reset}\n\n";
-sudo docker ps -a;
-printf "\n${text_Green}-Containers - Currently Running-----------------------------------${text_reset}\n\n";
-sudo docker ps;
-printf "\n\n";
+}
+
+docker_status;
