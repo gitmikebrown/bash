78726dfe2f955d547f752caaa987e925e162d9cf
mjbrow17 <mike.j.brown@outlook.com>
2024-08-18 21:37:50 -0700

    .PS update

--- Docker/docker_status.ps1
+++ Docker/docker_status.ps1
@@ -6,26 +6,27 @@
 ################################################################################################
 
 
-function docker_status(){
+function docker_status_ps(){
+    Write-Output "Switch to docker_status.sh"
     wsl
-    text_Green='\033[0;32m';
-    text_reset='\033[0m';
-    clear;
-    printf "\n\n${text_Green}-Networks-----------------------------------------------------${text_reset}\n\n";
-    sudo docker network ls; 
-    printf "\n\n${text_Green}-Volumes-----------------------------------------------------${text_reset}\n\n";
-    sudo docker volume ls; 
-    printf "\n${text_Green}-Images--------------------------------------------------------${text_reset}\n\n";
-    sudo docker images; 
-    printf "\n${text_Green}-Containers - All------------------------------------------------${text_reset}\n\n";
-    sudo docker ps -a;
-    printf "\n${text_Green}-Containers - Currently Running-----------------------------------${text_reset}\n\n";
-    sudo docker ps;
-    printf "\n\n";
+#    text_Green='\033[0;32m';
+#    text_reset='\033[0m';
+#    clear;
+#    printf "\n\n${text_Green}-Networks-----------------------------------------------------${text_reset}\n\n";
+#   sudo docker network ls; 
+#   printf "\n\n${text_Green}-Volumes-----------------------------------------------------${text_reset}\n\n";
+#   sudo docker volume ls; 
+#   printf "\n${text_Green}-Images--------------------------------------------------------${text_reset}\n\n";
+#   sudo docker images; 
+#   printf "\n${text_Green}-Containers - All------------------------------------------------${text_reset}\n\n";
+#    sudo docker ps -a;
+#   printf "\n${text_Green}-Containers - Currently Running-----------------------------------${text_reset}\n\n";
+#   sudo docker ps;
+#   printf "\n\n";
 
 }
 
-docker_status;
+docker_status_ps;
 
 
 
