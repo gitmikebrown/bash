87feee3375399f107d61f9781e3ed875316c6a85
mjbrow17 <mike.j.brown@outlook.com>
2024-09-21 01:21:05 -0700

    working on reboot script.  Need to keep working on it.

--- OS/LINUX/Ubuntu/system/ubuntu-system-restart.sh
+++ OS/LINUX/Ubuntu/system/ubuntu-system-restart.sh
@@ -17,17 +17,30 @@
 ################################################################################################
 
 function ubuntuReboot(){
+    
+    text_Red='\033[0;31m'
+    text_reset='\033[0m';
+    minutes=$1
+    current_system_time="Current system time: $(date +"%T")";
+    message="The compter will restart in ${minutes}"
 
-    minutes=0
-    message='The compter will restart in $minutes'
+    if [ -n "$minutes" ]; then    
+        if [ "$minutes" -eq 0 ]; then
 
-    if [ $minutes -eq 0 ]; then
-
-        #The best option
-        sudo reboot now
+            #reboot now
+            output=$(sudo reboot now 2>&1)
+            printf "\n${text_Red}${output}${text_reset}\n\n"
+            exit 1
+        else
+            echo "$current_system_time"
+            echo "Scheduling reboot in $minutes minutes with message: $message"
+            output=$(sudo shutdown -r +$minutes "$message" 2>&1)
+            printf "\n${text_Red}${output}${text_reset}\n\n";
+            exit 1
+        fi
     else
-        #20 min
-        sudo shutdown -r +$minutes $message
+        echo "Usage: $0 <minutes>"
+        exit 1
     fi
 }
-#ubuntuReboot;
+ubuntuReboot 20;
