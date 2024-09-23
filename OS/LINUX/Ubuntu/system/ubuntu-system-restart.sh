679ce4c11d4f6dbb5cba528c9088fdd8719ebe47
mjbrow17 <mike.j.brown@outlook.com>
2024-09-23 00:36:16 -0700

    more on the reboot script

--- OS/LINUX/Ubuntu/system/ubuntu-system-restart.sh
+++ OS/LINUX/Ubuntu/system/ubuntu-system-restart.sh
@@ -5,47 +5,78 @@
 # Description: 
 
 ################################################################################################
-####   Shutdown
-################################################################################################
-
-
-
-
-
-################################################################################################
 ####   Restart
 ################################################################################################
+
 #!/bin/bash
-function ubuntuReboot(){
+function ubuntuReboot() {
     
+
+    # Validate timeValue is a number or "0"
+    #if ! [[ "$timeValue" =~ ^[0-9]+$ ]]; then
+    #    echo $errorMessage
+    #    return 1
+    #fi
+
+
+    # Text colors
     text_Red='\033[0;31m'
-    text_reset='\033[0m';
-    minutes=$1
-    message=(
-       "The computer will restart in ${minutes}" 
-       "Current system time: $(date +"%a %Y-%m-%d %T")"
-       "Scheduled reboot time: $(date -d \"+${minutes} minutes\" +\"%a %Y-%m-%d %T\")"
-    )
+    text_Green='\033[0;32m'
+    text_reset='\033[0m'
+    
+    # The value coming in should be 20m or 20h or 20d for minutes, hours, or days.
+    incomingValue=$1
+    # Break the D,H,M off the end
+    timePeriod="${incomingValue: -1}"
+    timeValue="${incomingValue::-1}"
+    
+    # Error message to help users
+    errorMessage="Invalid time period. Use 'm' for minutes, 'h' for hours, or 'd' for days. Example 20m"
 
-    if [ -n "$minutes" ]; then    
-        if [ "$minutes" -eq 0 ]; then
-
-            #reboot now
-            output=$(sudo reboot now 2>&1)
-            printf "\n${text_Red}${output}${text_reset}\n\n"
-            #exit 1
+    # System time message
+    timeNowMessage="Current system time: $(date +'%a %Y-%m-%d %T %Z')"
+    
+    # Check to see if incoming value is null
+    if [ -z "$incomingValue" ]; then
+        read -p "Do you want to reboot now? (y/n): " choice1
+        if [[ "$choice1" =~ ^[Yy]$ ]]; then
+            sudo reboot now
         else
-            output=$(sudo shutdown -r +$minutes 2>&1)
-            printf "\n${text_Red} \
-            ${message[0]}\n \
-            ${message[1]}\n\ 
-            ${output} \
-            ${text_reset}\n\n"
-            #exit 1
+            echo $errorMessage
         fi
+        return 1
+    # Cancel the shutdown
+    elif [ "${incomingValue,,}" == "c" ]; then #The ,, converts the incomingValue to lowercase
+        printf "${text_Green}"
+        sudo shutdown -c
+        printf "${text_reset}"
+        return 0
+    # Immediate reboot
+    elif [ "$incomingValue" -eq 0 ]; then
+        printf "\n${text_Red}"
+        printf "${timeNowMessage}\n"
+        sudo reboot now
+        printf "${text_reset}"
+        return 0
     else
-        echo "Usage: ${0} <minutes>"
-        #exit 1
+        #  breakdown the time into minutes
+        case "$timePeriod" in
+            [dD]) restartInMinutes=$((timeValue * 24 * 60)) ;;  # 24 hours in a day, 60 min in an hour
+            [hH]) restartInMinutes=$((timeValue * 60)) ;; # 60 min in an hour
+            [mM]) restartInMinutes=$timeValue ;;
+            *) echo $errorMessage; return 1 ;;
+        esac
+        
+        # Set and run the restart
+        output=$(sudo shutdown -r +$restartInMinutes 2>&1 | sed "s/, use 'shutdown -c' to cancel.//")  # capture the message output and remove some text
+        
+        printf "\n${text_Red}"
+        printf "\t\tThe computer will restart in ${restartInMinutes} minutes.\n\n"
+        printf "\t\t${timeNowMessage}\n"
+        printf "\t\t${output}\n\n"
+        printf "\t\tUse 'ubuntuReboot c' to cancel.\n"
+        printf "${text_reset}\n"
     fi
 }
-ubuntuReboot 20;
+
+ubuntuReboot 20m
