a0b2f0f41320d493ed123015469fda0cefc15853
mjbrow17 <mike.j.brown@outlook.com>
2024-11-02 20:48:15 -0700

    Made it so that they can be run in powershell too.

--- Docker/builds/CLI/Ubuntu.sh
+++ Docker/builds/CLI/Ubuntu.sh
@@ -5,25 +5,35 @@
 # Description: 
 
 
-function dockerUbuntuUp(){
-
-       sudo docker run --name ubuntu-1 \
-        -p 8080:80 \
-        -p 2222:20 \
-        -d ubuntu
-
+function dockerUbuntuUp() {
+    if ($IsWindows) {
+        docker run --name ubuntu-1 `
+            -p 8080:80 `
+            -p 2222:20 `
+            -d ubuntu
+    } else {
+        sudo docker run --name ubuntu-1 \
+            -p 8080:80 \
+            -p 2222:20 \
+            -d ubuntu
+    }
 }
-dockerUbuntuUp;
+dockerUbuntuUp
 
 
 
-
-function dockerUbuntuDown(){
-
-    sudo docker stop ubuntu-1;
-    sudo docker container rm ubuntu-1;
-    #sudo docker volume rm ubuntu;
-    sudo docker image rm ubuntu;
-
+function dockerUbuntuDown() {
+    if ($IsWindows) {
+        docker stop ubuntu-1
+        docker container rm ubuntu-1
+        #docker volume rm ubuntu
+        docker image rm ubuntu
+    } else {
+        sudo docker stop ubuntu-1
+        sudo docker container rm ubuntu-1
+        #sudo docker volume rm ubuntu
+        sudo docker image rm ubuntu
+    }
 }
-dockerUbuntuDown;
+dockerUbuntuDown
+
